ca2b2a143f5d5fbea2f8e9542200dbf2ba0ad6e5
Ed Pichler <edpichler@gmail.com>
2010-09-16 09:41:35 -0300

    Backup.

--- eclipse workspace/OSGi Address Services Tests/src/test/java/com/edpichler/osgi/address/AspectJPAEntityTest.java
+++ eclipse workspace/OSGi Address Services Tests/src/test/java/com/edpichler/osgi/address/AspectJPAEntityTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import java.util.Properties;
 
 import org.junit.Test;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
 
 import com.edpichler.osgi.address.conf.AddressServicesConfiguration;
 import com.edpichler.osgi.jpa.address.Address;
@@ -13,23 +15,26 @@ public class AspectJPAEntityTest {
 
 	@Test
 	public void testCreateEntityManager() {
-		new Address().find(3); 
+		new Address().find(3);
 		System.out.println("passou");
 	}
+
 	@Test
 	public void testOverrideConf() {
 		AddressServicesConfiguration.setPersistenceUnitProperties(null);
-		new Address().find(3); 
+		new Address().find(3);
 		System.out.println("passou");
 	}
+
 	@Test
 	public void testOverrideConf2() {
-		//<property name="javax.persistence.jdbc.url" value="jdbc:derby:address;create=true" />
-		
+		// <property name="javax.persistence.jdbc.url"
+		// value="jdbc:derby:address;create=true" />
+
 		Properties p = new Properties();
 		p.put("javax.persistence.jdbc.url", "jdbc:derby:anydb;create=true");
 		AddressServicesConfiguration.setPersistenceUnitProperties(p);
-		new Address().find(3); 
+		new Address().find(3);
 		System.out.println("passou");
-	}
-} 
+	}	
+}
